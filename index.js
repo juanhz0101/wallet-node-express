4fa11124085394ff21e7ffc0558f5db6ca0c372c
Juan Pablo Hurtado <juanpablo.h02@gmail.com>
2019-02-26 23:55:29 -0500

    se configura puerto del servicio para operacion dinamica

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const bodyParser = require('body-parser');
 /**
  * Inicializacion de servicios
  */
+
+const port = process.env.PORT || 3000;
+
 const app = express();
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log("El servidor está inicializado en el puerto 3000");
 });
 
